2b01bb58121bf9396f9388503c1d0e1984b40030
Kiash254 <codewitkiash@gmil.com>
2022-05-10 23:56:37 +0300

    registerd models in the admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+admin.site.register(Blog)
+admin.site.register(BlogCategory)
+admin.site.register(BlogMedia)
+admin.site.register(Leaders)
+admin.site.resister(Events)
